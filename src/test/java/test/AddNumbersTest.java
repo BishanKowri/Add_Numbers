3300f395e302e78412eebe300045ee4ce4285391
BTP <BTP@SAG-HQ6X2G2.mshome.net>
2022-04-26 11:51:03 +0530

    Added the initial test case class

---
+++ src/test/java/test/AddNumbersTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class AddNumbersTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
