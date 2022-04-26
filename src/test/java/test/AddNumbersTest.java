0b77e390935717d0c78b796636d7d825b3475d70
BTP <BTP@SAG-HQ6X2G2.mshome.net>
2022-04-26 12:08:49 +0530

    initial sum method

--- src/test/java/test/AddNumbersTest.java
+++ src/test/java/test/AddNumbersTest.java
@@ -1,13 +1,17 @@
 package test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class AddNumbersTest {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	 public void testSum() {  
+        assertEquals(25,sum(10, 15));  
+    }
+
+	private static long sum(int i, int j) {
+		return i+j;
+	}  
 }
