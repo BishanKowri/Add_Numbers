d3256b756f264d901f7c780c218af9345d0d9b27
BTP <BTP@SAG-HQ6X2G2.mshome.net>
2022-04-26 12:35:35 +0530

    Added more testcases

--- src/test/java/test/AddNumbersTest.java
+++ src/test/java/test/AddNumbersTest.java
@@ -8,9 +8,18 @@ public class AddNumbersTest {
 	
 	@Test
 	 public void testSum() {  
-		assertEquals(0,sum(" "));
-		assertEquals(10,sum("10 "));
-        assertEquals(25,sum("10, 15"));  
+		assertEquals(0,sum(" ")); 
+		assertEquals(10,sum("10 ")); //trims the trailing space 
+		assertEquals(10,sum("1 0 ")); //removes the space in between the numbers
+		assertEquals(10,sum("1 0, ")); //ignores the blank string while trying to add 
+		assertEquals(10,sum("1 0, ,,")); //ignores all blank string while trying to add 
+		assertEquals(20,sum("1 0, ,,10")); //ignores the blank string in between while trying to add 
+		assertEquals(20,sum(",,,1 0, ,,1 0")); //ignores the blank string in the beginning and in between while trying to add 
+        assertEquals(25,sum("10, 15")); //trims the leading space 
+        assertEquals(72484,sum("10, 15, 40,56,20,98,123,65,23,45"
+        		+ ",51,6,561,56,51651,5,6515,5,51,5615,561,651,5156,156,"
+        		+ "165,1,6,156,65,165,,230,23,023,23,23,23,23,0,23")); //Adds all Numbers
+        
     }
 
 	private static long sum(String numbers) {
@@ -20,7 +29,10 @@ public class AddNumbersTest {
 		}
 		String numArray[] = numbers.split(",");
 		for (int i=0;i<numArray.length;i++) {
-			int num = Integer.parseInt(numArray[i].trim());
+			if (numArray[i].isBlank()) {
+				continue;
+			}
+			long num = Long.parseLong(numArray[i].replaceAll("\\s", "").trim());
 			sum += num;
 		}
 		return sum;
