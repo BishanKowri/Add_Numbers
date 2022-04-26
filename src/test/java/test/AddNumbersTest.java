cbda8994584c422c3e7694d2e7eb4863704f4aca
BTP <BTP@SAG-HQ6X2G2.mshome.net>
2022-04-26 12:13:47 +0530

    Changed the method sum to accept string. first test case

--- src/test/java/test/AddNumbersTest.java
+++ src/test/java/test/AddNumbersTest.java
@@ -8,10 +8,17 @@ public class AddNumbersTest {
 	
 	@Test
 	 public void testSum() {  
-        assertEquals(25,sum(10, 15));  
+        assertEquals(25,sum("10, 15"));  
     }
 
-	private static long sum(int i, int j) {
-		return i+j;
+	private static long sum(String numbers) {
+		int sum = 0;
+		String numArray[] = numbers.split(",");
+		
+		for (int i=0;i<numArray.length;i++) {
+			int num = Integer.parseInt(numArray[i].trim());
+			sum += num;
+		}
+		return sum;
 	}  
 }
