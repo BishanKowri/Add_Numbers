5549fe697585cc1395438f5e375c57c679303e0b
BTP <BTP@SAG-HQ6X2G2.mshome.net>
2022-04-26 12:44:13 +0530

    Supporting multiple delimiter , and \n

--- src/test/java/test/AddNumbersTest.java
+++ src/test/java/test/AddNumbersTest.java
@@ -11,15 +11,16 @@ public class AddNumbersTest {
 		assertEquals(0,sum(" ")); 
 		assertEquals(10,sum("10 ")); //trims the trailing space 
 		assertEquals(10,sum("1 0 ")); //removes the space in between the numbers
-		assertEquals(10,sum("1 0, ")); //ignores the blank string while trying to add 
+        assertEquals(25,sum("10, 15")); //trims the space in between 
+        assertEquals(10,sum("1 0, ")); //ignores the blank string while trying to add 
 		assertEquals(10,sum("1 0, ,,")); //ignores all blank string while trying to add 
 		assertEquals(20,sum("1 0, ,,10")); //ignores the blank string in between while trying to add 
 		assertEquals(20,sum(",,,1 0, ,,1 0")); //ignores the blank string in the beginning and in between while trying to add 
-        assertEquals(25,sum("10, 15")); //trims the leading space 
         assertEquals(72484,sum("10, 15, 40,56,20,98,123,65,23,45"
         		+ ",51,6,561,56,51651,5,6515,5,51,5615,561,651,5156,156,"
         		+ "165,1,6,156,65,165,,230,23,023,23,23,23,23,0,23")); //Adds all Numbers
-        
+        assertEquals(50,sum("10\n 15,25")); //trims the space in between 
+        assertEquals(1,sum("1,\n"));
     }
 
 	private static long sum(String numbers) {
@@ -27,7 +28,7 @@ public class AddNumbersTest {
 		if (numbers.isBlank()) {
 			return sum;
 		}
-		String numArray[] = numbers.split(",");
+		String numArray[] = numbers.split(",|\\n");
 		for (int i=0;i<numArray.length;i++) {
 			if (numArray[i].isBlank()) {
 				continue;
