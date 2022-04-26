19d08c6beae50dde828e107f09f5d2d55e96f413
BTP <BTP@SAG-HQ6X2G2.mshome.net>
2022-04-26 12:22:49 +0530

    Added 2 more testcases

--- src/test/java/test/AddNumbersTest.java
+++ src/test/java/test/AddNumbersTest.java
@@ -8,13 +8,17 @@ public class AddNumbersTest {
 	
 	@Test
 	 public void testSum() {  
+		assertEquals(0,sum(" "));
+		assertEquals(10,sum("10 "));
         assertEquals(25,sum("10, 15"));  
     }
 
 	private static long sum(String numbers) {
 		int sum = 0;
+		if (numbers.isBlank()) {
+			return sum;
+		}
 		String numArray[] = numbers.split(",");
-		
 		for (int i=0;i<numArray.length;i++) {
 			int num = Integer.parseInt(numArray[i].trim());
 			sum += num;
